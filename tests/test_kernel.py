3adfa806582d82e538f23dde57089e0c104f17de
fy1214 <282598660@qq.com>
2025-07-03 19:14:43 +0800

    [add] new cvt2fp4 instruction

--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -2,14 +2,14 @@ from mimetypes import add_type
 
 import torch
 
-from fp4_gemm import _C
+import fp4_gemm
 
 if __name__ == '__main__':
     m, n = 1024, 512
 
-    x = torch.rand((m, n), device='cuda', dtype='torch.bfloat16')
-    x_scale = torch.tensor([1], device='cuda', dtype='torch.float32')
+    x = torch.rand((m, n), device='cuda', dtype=torch.bfloat16)
+    x_scale = torch.tensor([1], device='cuda', dtype=torch.float32)
 
-    out = torch.empty((m, n // 8), device='cuda', dtype='torch.int32')
-    out_scale = torch.empty((m, n // 8), device='cuda', dtype='torch.float8_e4m3fn')
-    _C.scaled_fp4_quant(out, x, out_scale, x_scale)
+    out = torch.empty((m, n // 8), device='cuda', dtype=torch.int32)
+    out_scale = torch.empty((m, n // 8), device='cuda', dtype=torch.float8_e4m3fn)
+    torch.ops.fp4_gemm.scaled_fp4_quant(out, x, out_scale, x_scale)

c2db514013fce4c50fb293ea05f3f1740a191946
fy1214 <282598660@qq.com>
2025-07-04 18:53:55 +0800

    [add]add dequant kernel

--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -7,12 +7,14 @@ import fp4_gemm
 if __name__ == '__main__':
     m, n = 128, 128
 
-    x = torch.rand((m, n), device='cuda', dtype=torch.bfloat16)
-    x_scale = torch.tensor([1], device='cuda', dtype=torch.float32)
+    x = torch.rand((m, n), device='cuda', dtype=torch.half)
 
     out = torch.empty((m, n // 8), device='cuda', dtype=torch.int32)
     out_scale = torch.empty((m, n // 8), device='cuda', dtype=torch.float8_e4m3fn)
-    fp4_gemm.scaled_fp4_quant(out, x, out_scale, x_scale)
+    fp4_gemm.scaled_fp4_quant(out, x, out_scale)
 
     print(out)
     print(out_scale)
+
+    x_n = torch.empty_like(x)
+    fp4_gemm.scaled_fp4_dequant(out, out_scale, x_n)

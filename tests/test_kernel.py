6403bd27fc8b8ddb672afd69f79e6d7b77eaf11c
fy1214 <282598660@qq.com>
2025-07-04 11:19:41 +0800

    [add]fp4 quant kernel fix

--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -5,7 +5,7 @@ import torch
 import fp4_gemm
 
 if __name__ == '__main__':
-    m, n = 1024, 512
+    m, n = 128, 128
 
     x = torch.rand((m, n), device='cuda', dtype=torch.bfloat16)
     x_scale = torch.tensor([1], device='cuda', dtype=torch.float32)
@@ -13,3 +13,6 @@ if __name__ == '__main__':
     out = torch.empty((m, n // 8), device='cuda', dtype=torch.int32)
     out_scale = torch.empty((m, n // 8), device='cuda', dtype=torch.float8_e4m3fn)
     fp4_gemm.scaled_fp4_quant(out, x, out_scale, x_scale)
+
+    print(out)
+    print(out_scale)
